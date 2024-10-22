83337af0edfa02250f92db59f02c37f713eaaa25
Alonso del Arte <alonso.delarte@gmail.com>
2024-10-22 11:53:46 -0400

    Passing test that chooseCurrency( ) gives enough distinct currencies

--- src/currency/CurrencyChooser.scala
+++ src/currency/CurrencyChooser.scala
@@ -3,7 +3,49 @@ package currency
 import java.util.Currency
 import java.util.function.Predicate
 
+import scala.collection.JavaConverters._
+import scala.util.Random
+
 object CurrencyChooser {
+
+  private val ALL_JRE_RECOGNIZED_CURRENCIES: Set[Currency] =
+    Currency.getAvailableCurrencies.asScala.toSet
+
+  private val PSEUDOCURRENCIES: Set[Currency] =
+    ALL_JRE_RECOGNIZED_CURRENCIES.filter(_.getDefaultFractionDigits < 0)
+
+  private def isHistoricalCurrency(currency: Currency): Boolean = {
+    val name = currency.getDisplayName
+    name.contains("\u002818") || name.contains("\u002819") ||
+      name.contains("\u002820")
+  }
+
+  private val HISTORICAL_CURRENCIES: Set[Currency] =
+    ALL_JRE_RECOGNIZED_CURRENCIES.filter(isHistoricalCurrency)
+
+  private val EURO_REPLACED_CURRENCY_CODES: Set[String] = Set("ADP", "ATS",
+    "BEF", "CYP", "DEM", "EEK", "ESP", "FIM", "FRF", "GRD", "IEP", "ITL", "LUF",
+    "MTL", "NLG", "PTE", "SIT")
+
+  private val EURO_REPLACED_CURRENCIES: Set[Currency] =
+    EURO_REPLACED_CURRENCY_CODES.map(Currency.getInstance)
+
+  private val OTHER_EXCLUSION_CODES: Set[String] = Set("AYM", "BGL", "BOV",
+    "CHE", "CHW", "COU", "GWP", "MGF", "MXV", "SRG", "STN", "TPE", "USN", "USS",
+    "UYI", "VED", "ZWN")
+
+  private val OTHER_EXCLUDED_CURRENCIES: Set[Currency] =
+    OTHER_EXCLUSION_CODES.map(Currency.getInstance)
+
+  private val AGGREGATE_EXCLUSIONS: Set[Currency] = HISTORICAL_CURRENCIES ++
+    EURO_REPLACED_CURRENCIES ++ OTHER_EXCLUDED_CURRENCIES
+
+  private val CURRENCIES: Set[Currency] = ALL_JRE_RECOGNIZED_CURRENCIES --
+    AGGREGATE_EXCLUSIONS
+
+  private val CURRENCIES_ARRAY: Array[Currency] = CURRENCIES.toArray
+
+  private val TOTAL_NUMBER_OF_AVAILABLE_CURRENCIES: Int = CURRENCIES_ARRAY.length
 
   // TODO: Write tests for this
   def getSuitableCurrencies: Set[Currency] = Set(Currency.getInstance("XTS"))
@@ -12,7 +54,10 @@ object CurrencyChooser {
   def choosePseudoCurrency: Currency = Currency.getInstance("USD")
 
   // TODO: Write tests for this
-  def chooseCurrency: Currency = Currency.getInstance("USD")
+  def chooseCurrency: Currency = {
+    val index = Random.nextInt(TOTAL_NUMBER_OF_AVAILABLE_CURRENCIES)
+    CURRENCIES_ARRAY(index)
+  }
 
   // TODO: Write tests for this
   def chooseCurrency(fractionDigits: Int): Currency =
