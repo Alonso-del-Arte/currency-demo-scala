970ddb727a66db2c10abf508e56bf811dd57de12
Alonso del Arte <alonso.delarte@gmail.com>
2024-10-21 21:36:30 -0400

    Preparing some lists for tests

--- test/currency/CurrencyChooserTest.scala
+++ test/currency/CurrencyChooserTest.scala
@@ -1,7 +1,52 @@
 package currency
 
+import java.util.{ArrayList, Arrays, Currency}
+import java.util.function.Predicate
+
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
+
+import scala.collection.JavaConverters._
+import scala.util.Random
+
+object CurrencyChooserTest {
+
+  private val ALL_JRE_RECOGNIZED_CURRENCIES: Set[Currency] =
+    Currency.getAvailableCurrencies.asScala.toSet
+
+  private val PSEUDOCURRENCIES: Set[Currency] =
+    ALL_JRE_RECOGNIZED_CURRENCIES.filter(_.getDefaultFractionDigits < 0)
+
+  private def isHistoricalCurrency(currency: Currency): Boolean = {
+    val name = currency.getDisplayName
+    name.contains("\u002818") || name.contains("\u002819") ||
+      name.contains("\u002820")
+  }
+
+  private val HISTORICAL_CURRENCIES: Set[Currency] =
+    ALL_JRE_RECOGNIZED_CURRENCIES.filter(isHistoricalCurrency)
+
+  private val EURO_REPLACED_CURRENCY_CODES: Set[String] = Set("ADP", "ATS",
+    "BEF", "CYP", "DEM", "EEK", "ESP", "FIM", "FRF", "GRD", "IEP", "ITL", "LUF",
+    "MTL", "NLG", "PTE", "SIT")
+
+  private val EURO_REPLACED_CURRENCIES: Set[Currency] =
+    EURO_REPLACED_CURRENCY_CODES.map(Currency.getInstance)
+
+  private val OTHER_EXCLUSION_CODES: Set[String] = Set("AYM", "BGL", "BOV",
+    "CHE", "CHW", "COU", "GWP", "MGF", "MXV", "SRG", "STN", "TPE", "USN", "USS",
+    "UYI", "VED", "ZWN")
+
+  private val OTHER_EXCLUDED_CURRENCIES: Set[Currency] =
+    OTHER_EXCLUSION_CODES.map(Currency.getInstance)
+
+  private val AGGREGATE_EXCLUSIONS: Set[Currency] = HISTORICAL_CURRENCIES ++
+    EURO_REPLACED_CURRENCIES ++ OTHER_EXCLUDED_CURRENCIES
+
+  private val CURRENCIES: Set[Currency] = ALL_JRE_RECOGNIZED_CURRENCIES -
+    AGGREGATE_EXCLUSIONS
+
+}
 
 class CurrencyChooserTest {
 
