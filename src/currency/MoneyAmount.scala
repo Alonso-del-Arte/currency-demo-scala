0be001c3fcf52332aeca525cb095c65d4b653aa3
Alonso del Arte <alonso.delarte@gmail.com>
2024-10-23 23:32:12 -0400

    Added a couple of field stubs

--- src/currency/MoneyAmount.scala
+++ src/currency/MoneyAmount.scala
@@ -3,5 +3,7 @@ package currency
 import java.util.{Currency, Locale}
 
 class MoneyAmount(val fullAmountInCents: Long, val currency: Currency) {
+  val units: Long = -1 // TODO: Write tests for this
+  val subdivisions: Short = -1 // TODO: Write tests for this
 
 }
