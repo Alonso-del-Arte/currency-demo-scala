abf7af7d694aafe1d8255e1279af7687fb6360d2
Alonso del Arte <alonso.delarte@gmail.com>
2024-10-23 23:37:30 -0400

    Arithmetic stubs

--- src/currency/MoneyAmount.scala
+++ src/currency/MoneyAmount.scala
@@ -6,4 +6,19 @@ class MoneyAmount(val fullAmountInCents: Long, val currency: Currency) {
   val units: Long = -1 // TODO: Write tests for this
   val subdivisions: Short = -1 // TODO: Write tests for this
 
+  // TODO: Write tests for this
+  def +(addend: MoneyAmount): MoneyAmount = this
+
+  // TODO: Write tests for this
+  def unary_-(): MoneyAmount = this
+
+  // TODO: Write tests for this
+  def -(subtrahend: MoneyAmount): MoneyAmount = this
+
+  // TODO: Write tests for this
+  def *(multiplicand: Int): MoneyAmount = this
+
+  // TODO: Write tests for this
+  def /(divisor: Int): MoneyAmount = this
+
 }
