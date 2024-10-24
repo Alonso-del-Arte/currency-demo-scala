211cc96b6d9228db68ff4edc5c8048364ef99e14
Alonso del Arte <alonso.delarte@gmail.com>
2024-10-23 23:40:10 -0400

    unitsDivisor( ) stub

--- src/currency/MoneyAmount.scala
+++ src/currency/MoneyAmount.scala
@@ -2,6 +2,13 @@ package currency
 
 import java.util.{Currency, Locale}
 
+object MoneyAmount {
+
+  // TODO: Write tests for this
+  private[currency] def unitsDivisor(currency: Currency): Int = -1
+
+}
+
 class MoneyAmount(val fullAmountInCents: Long, val currency: Currency) {
   val units: Long = -1 // TODO: Write tests for this
   val subdivisions: Short = -1 // TODO: Write tests for this
