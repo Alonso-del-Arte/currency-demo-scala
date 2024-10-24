9605b3b418ad2c73c69a65ea332576880f2c6426
Alonso del Arte <alonso.delarte@gmail.com>
2024-10-23 23:46:11 -0400

    Declared a couple of currencies to use in several tests

--- test/currency/MoneyAmountTest.scala
+++ test/currency/MoneyAmountTest.scala
@@ -1,7 +1,19 @@
 package currency
 
+import java.util.{Currency, Locale}
+
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
+
+object MoneyAmountTest {
+
+  private val DOLLARS: Currency = Currency.getInstance(Locale.US)
+
+  private val EUROS: Currency = Currency.getInstance("EUR")
+
+  private val YEN: Currency = Currency.getInstance(Locale.JAPAN)
+
+}
 
 class MoneyAmountTest {
 
