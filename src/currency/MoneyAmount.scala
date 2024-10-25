3f581599515d781e14f7c031419d8ba4891970a1
Alonso del Arte <alonso.delarte@gmail.com>
2024-10-25 00:10:52 -0400

    Passing toString( ) test

--- src/currency/MoneyAmount.scala
+++ src/currency/MoneyAmount.scala
@@ -13,6 +13,14 @@ class MoneyAmount(val fullAmountInCents: Long, val currency: Currency) {
   val units: Long = -1 // TODO: Write tests for this
   val subdivisions: Short = -1 // TODO: Write tests for this
 
+  override def toString: String = {
+    val symbol = Currency.getInstance(Locale.US).getSymbol
+    val numStr = this.fullAmountInCents.toString
+    val len = numStr.length
+    val dot = len - 2
+    s"$symbol${numStr.substring(0, dot)}.${numStr.substring(dot, len)}"
+  }
+
   // TODO: Write tests for this
   def +(addend: MoneyAmount): MoneyAmount = this
 
