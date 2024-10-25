3c9ed1173a8c93edcda77b1407512d283cee38fd
Alonso del Arte <alonso.delarte@gmail.com>
2024-10-24 23:55:35 -0400

    Failing toString( ) test

--- test/currency/MoneyAmountTest.scala
+++ test/currency/MoneyAmountTest.scala
@@ -5,6 +5,8 @@ import java.util.{Currency, Locale}
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 
+import scala.util.Random
+
 object MoneyAmountTest {
 
   private val DOLLARS: Currency = Currency.getInstance(Locale.US)
@@ -16,5 +18,18 @@ object MoneyAmountTest {
 }
 
 class MoneyAmountTest {
+
+  @Test def testToString(): Unit = {
+    val fullAmountInCents = Random.nextInt(Short.MaxValue) + Byte.MaxValue
+    val amount = new MoneyAmount(fullAmountInCents, MoneyAmountTest.DOLLARS)
+    val symbol = MoneyAmountTest.DOLLARS.getSymbol
+    val numStr = Integer.toString(fullAmountInCents)
+    val len = numStr.length
+    val dot = len - 2
+    val expected =
+      s"$symbol${numStr.substring(0, dot)}.${numStr.substring(dot, len)}"
+    val actual = amount.toString
+    assertEquals(expected, actual)
+  }
 
 }
