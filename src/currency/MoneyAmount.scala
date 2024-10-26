cb193a91c708aa58b087e970e92196e127c1c459
Alonso del Arte <alonso.delarte@gmail.com>
2024-10-25 23:47:01 -0400

    Passing units test for when currency has no subunits

--- src/currency/MoneyAmount.scala
+++ src/currency/MoneyAmount.scala
@@ -19,7 +19,7 @@ object MoneyAmount {
 }
 
 class MoneyAmount(val fullAmountInCents: Long, val currency: Currency) {
-  val units: Long = -1 // TODO: Write tests for this
+  val units: Long = this.fullAmountInCents // TODO: Write tests for this
   val subdivisions: Short = -1 // TODO: Write tests for this
 
   override def toString: String = {
