0530b2620a8f2aa9b00a0d59a3028e856ff4667b
Alonso del Arte <alonso.delarte@gmail.com>
2024-10-28 20:39:39 -0400

    Added parameters

--- src/currency/CurrencyConversionNeededException.scala
+++ src/currency/CurrencyConversionNeededException.scala
@@ -1,4 +1,7 @@
 package currency
 
 @SerialVersionUID(4559181705386131456L)
-class CurrencyConversionNeededException extends RuntimeException
+class CurrencyConversionNeededException(message: String = "",
+                                        val amountA: MoneyAmount,
+                                        val amountB: MoneyAmount)
+  extends RuntimeException
