fd80d868261f4e8b29c41dfbfa00b22631719de6
Alonso del Arte <alonso.delarte@gmail.com>
2024-10-28 21:19:17 -0400

    Passing default message test

--- src/currency/CurrencyConversionNeededException.scala
+++ src/currency/CurrencyConversionNeededException.scala
@@ -3,5 +3,6 @@ package currency
 @SerialVersionUID(4559181705386131456L)
 class CurrencyConversionNeededException(val amountA: MoneyAmount,
                                         val amountB: MoneyAmount,
-                                        message: String = "")
-  extends RuntimeException
+                                        message: String =
+                                        "Currency conversion needed for the two amounts")
+  extends RuntimeException(message)
